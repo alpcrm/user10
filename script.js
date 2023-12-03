a4dc9ddfe98f6ed730ccc39560642bb60f0b7712
alpcrm <152732237+alpcrm@users.noreply.github.com>
2023-12-04 00:20:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function calculateRevenue() {
-  const monthlyViewers = parseFloat(document.getElementById('monthlyViewers').value);
-  const totalAdImpressions = parseFloat(document.getElementById('totalAdImpressions').value);
 
-  const revenue = (monthlyViewers / 10) * totalAdImpressions; // Equation updated
-  const resultElement = document.getElementById('monthlyRevenue');
-  
-  if (!isNaN(revenue)) {
-    resultElement.textContent = `Monthly Revenue: $${revenue.toFixed(2)}`;
-  } else {
-    resultElement.textContent = 'Please enter valid inputs';
-  }
-}
